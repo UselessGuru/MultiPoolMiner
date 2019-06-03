88cb4482c3bb9587c0e55ca2add22b434aac2fb4
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-06-03 20:30:38 +0200

    Miner files: Remove code for UseDeviceNameForStatsFileNaming

--- MultiPoolMiner/MinersLegacy/NVIDIA-CcminerTpruvot_v2.3.1.ps1
+++ MultiPoolMiner/MinersLegacy/NVIDIA-CcminerTpruvot_v2.3.1.ps1
@@ -79,7 +79,7 @@ else {
         #"whirlpoolx"    = "" #whirlpoolx
         "x11evo"        = "" #X11evo
         "x12"           = "" #X12
-        "x16r"          = "" #X16R
+# Other free miners are faster        "x16r"          = "" #X16R
         #"X16s"         = "" #X16S
         #"x17"          = "" #x17
         "zr5"           = "" #zr5
@@ -119,12 +119,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
     $Miner_Port = $Config.APIPort + ($Miner_Device | Select-Object -First 1 -ExpandProperty Index) + 1
 
     $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {if ($_ -eq "monero") {$Algorithm_Norm = "cryptonight7"}<#TempFix, monero is no longer using cn7#> else  {$Algorithm_Norm = Get-Algorithm $_}; $_} | Where-Object {$Pools.$Algorithm_Norm.Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
-        if ($Config.UseDeviceNameForStatsFileNaming) {
-            $Miner_Name = (@($Name) + @(($Miner_Device.Model_Norm | Sort-Object -unique | ForEach-Object {$Model_Norm = $_; "$(@($Miner_Device | Where-Object Model_Norm -eq $Model_Norm).Count)x$Model_Norm"}) -join '_') | Select-Object) -join '-'
-        }
-        else {
-            $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-        }
+        $Miner_Name = (@($Name) + @(($Miner_Device.Model_Norm | Sort-Object -unique | ForEach-Object {$Model_Norm = $_; "$(@($Miner_Device | Where-Object Model_Norm -eq $Model_Norm).Count)x$Model_Norm"}) -join '_') | Select-Object) -join '-'
 
         #Get parameters for active miner devices
         if ($Miner_Config.Parameters.$Algorithm_Norm) {
